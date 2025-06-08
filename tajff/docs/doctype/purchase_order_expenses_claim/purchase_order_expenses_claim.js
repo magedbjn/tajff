4581a84050af3dfe5febae3171a4b55cecf6da5a
IT-Tajff <it@tajff.com>
2025-06-08 04:40:14 +0300

    change path report

--- tajff/docs/doctype/purchase_order_expenses_claim/purchase_order_expenses_claim.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Maged BAjandooh and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Purchase Order Expenses Claim", {
-// 	refresh(frm) {
-
-// 	},
-// });
