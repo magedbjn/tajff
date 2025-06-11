8213860ebf9e0c7ca4e55b23da7139476d08f375
IT-Tajff <it@tajff.com>
2025-06-11 18:43:03 +0300

    Update files 2

--- tajff/override/gratuity.py
+++ tajff/override/gratuity.py
@@ -1,26 +1,25 @@
 import frappe
 from frappe import _, bold
-from frappe.utils import flt, get_datetime, get_link_to_form
-
+from frappe.utils import get_datetime, get_link_to_form
 from hrms.payroll.doctype.gratuity.gratuity import Gratuity
 
 
-from datetime import datetime
 class Gratuity_new(Gratuity):
-	def get_total_working_days(self) -> float:
-		## We don't want to deduct the days of Absent and LWP leave from days of end of service
-		date_of_joining, relieving_date = frappe.db.get_value(
-			"Employee", self.employee, ["date_of_joining", "relieving_date"]
-		)
-		if not relieving_date:
-			frappe.throw(
-				_("Please set Relieving Date for employee: {0}").format(
-					bold(get_link_to_form("Employee", self.employee))
-				)
-			)
+    def get_total_working_days(self) -> float:
+        # We don't want to deduct the days of Absent and LWP leave from days of end of service
+        date_of_joining, relieving_date = frappe.db.get_value(
+            "Employee", self.employee, ["date_of_joining", "relieving_date"]
+        )
+        if not relieving_date:
+            frappe.throw(
+                _("Please set Relieving Date for employee: {0}").format(
+                    bold(get_link_to_form("Employee", self.employee))
+                )
+            )
 
-		total_working_days = (get_datetime(relieving_date) - get_datetime(date_of_joining)).days
-		
+        total_working_days = (get_datetime(relieving_date) - get_datetime(date_of_joining)).days
+        return total_working_days
+    		
 		# payroll_based_on = frappe.db.get_single_value("Payroll Settings", "payroll_based_on") or "Leave"
         # if payroll_based_on == "Leave":
 		# 	total_lwp = self.get_non_working_days(relieving_date, "On Leave")
--- tajff/override/material_request.py
+++ tajff/override/material_request.py
@@ -3,23 +3,30 @@ from frappe import _
 
 @frappe.whitelist()
 def collect_similar_items(doc):
-    item_qty = {}
+    # Initialize data structures
+    total_quantities = {}
     items_to_delete = []
 
+    # First pass: Collect quantities and identify duplicates
     for item in doc.items:
         item_code = item.item_code
-
-        if item_code in item_qty:
-            item_qty[item_code] = item_qty[item_code] + item_qty
+        
+        if item_code in total_quantities:
+            # Accumulate quantity for duplicate items
+            total_quantities[item_code] += item.qty
+            # Mark duplicate item for removal
             items_to_delete.append(item)
         else:
-            item_qty[item_code] = item_code
+            # Initialize for new item code
+            total_quantities[item_code] = item.qty
 
-    for item in items_to_delete:
-        doc.items_remove(item)
+    # Second pass: Remove duplicate items (in reverse order)
+    for item in reversed(items_to_delete):
+        doc.remove(item)
 
-    for item_code, qty in item_qty.items():
-        for item in doc.items:
-            if item.item_code == item_code:
-                item_qty = qty
-                break
+    # Third pass: Update quantities for remaining items
+    for item in doc.items:
+        if item.item_code in total_quantities:
+            item.qty = total_quantities[item.item_code]
+            # Remove from dict to prevent re-updating
+            del total_quantities[item.item_code]
